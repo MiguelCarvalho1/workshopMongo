62c8e6cbcaf0ddfaecb594573bfd44c110dda282
migue <miguelangelosilvacarvalho96@gmail.com>
2024-01-15 14:59:05 +0000

    Projeção dos dados do autor com DTO
    Checklist:
     Criar AuthorDTO
     Refatorar Post
     Refatorar a carga inicial do banco de dados
    o IMPORTANTE: agora é preciso persistir os objetos User antes de relacionar

--- src/main/java/com/miguel/workshopmongo/config/Instantiation.java
+++ src/main/java/com/miguel/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.miguel.workshopmongo.config;
 
 import com.miguel.workshopmongo.domain.Post;
 import com.miguel.workshopmongo.domain.User;
+import com.miguel.workshopmongo.dto.AuthorDTO;
 import com.miguel.workshopmongo.repository.PostRepository;
 import com.miguel.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,8 +34,8 @@ public class Instantiation implements CommandLineRunner {
 
         postRepository.deleteAll();
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia !!", "Acordei feliz hoje!!", maria);
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!!", new AuthorDTO(maria) );
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia !!", "Acordei feliz hoje!!", new AuthorDTO(maria));
         postRepository.saveAll(Arrays.asList(post1,post2));
 
 
